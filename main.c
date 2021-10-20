16efa1d0ee51888ac4302f148e76e5eae1806702
tonTed <yddetco@gmail.com>
2021-10-20 08:58:11 -0400

    test for do testing

--- main.c
+++ main.c
@@ -6,39 +6,69 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 10:01:45 by tblanco           #+#    #+#             */
-/*   Updated: 2021/10/19 19:06:45 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/10/20 00:29:18 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "include/ft_printf.h"
 #include "include/ft_colors.h"
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+char	*ft_getstdin(int (*f)(const char*, ...))
+{
+	char	*buffer;
+	pid_t	pid;
+	int		status;
+	int		fd[2];
+	int		char_read;
+
+	buffer = (char *)malloc(sizeof(char) * ((int)BUFSIZ + 1));
+	pipe(fd);
+	pid = fork();
+	if (pid)
+	{
+		wait(&status);
+		char_read = read(fd[0], buffer, BUFSIZ);
+		buffer[char_read] = '\0';
+		return(buffer);
+	}
+	else
+	{
+		close(fd[0]);
+		dup2(fd[1], 1);
+		close(fd[1]);
+		f("%c\n", 'a');
+		close(1);
+	}
+	return (NULL);
+}
 
 int main(void)
 {
-	// int ret_ft;
-	// int ret_printf;
-	// int pos = 42;
-	// void *ptr = &pos;
+	// int		err	= 0;
+	// int		test = 0;
+	int		total_err = 0;
+	int		total_test = 0;
+	char	*_ft_printf;
+	char	*_printf;
 	
-	// printf("\n---------- TESTING START ----------");
-	// printf("\n********** Char **********\n");
-	// ret_ft = ft_printf("c: %c\n", '#');
-	// ret_printf = printf("c: %c\n", '#');
-	// printf("return ft : %d - %d : printf\n", ret_ft, ret_printf);
-	// printf("\n********** String **********\n");
-	// ret_ft = ft_printf("s: %s\n", "allo");
-	// ret_printf = printf("s: %s\n", "allo");
-	// printf("return ft : %d - %d : printf\n", ret_ft, ret_printf);
-	// printf("\n********** int (d & i) **********\n");
-	// ret_ft = ft_printf("d: %d - i: %i - d: %d - i: %i\n", pos, pos, pos*-1, pos*-1);
-	// ret_printf = printf("d: %d - i: %i - d: %d - i: %i\n", pos, pos, pos*-1, pos*-1);
-	// printf("return ft : %d - %d : printf\n", ret_ft, ret_printf);
-	// printf("\n********** address (p) **********\n");
-	// ret_ft = ft_printf("d: %p\n", ptr);
-	// ret_printf = printf("d: %p\n", ptr);
+	printf("\n" BBLU "---------- TESTING START ----------" reset);
+	printf("\n" UCYN "### %%c:\n" reset);
 
+	_printf = ft_getstdin(&printf);
+	_ft_printf = ft_getstdin(&printf);
+	if (strcmp(_printf, _ft_printf))
+		printf("expected: %s | %s :return\n", _printf, _ft_printf);
+	
+	if (total_err)
+		printf("\n" BRED "TOTAL ERRORS: %d/%d :(" reset "\n", total_err, total_test);
+	else
+		printf("\n" BGRN "CONGRATS!!! YOU SUCCED %d TESTS :)" reset "\n", total_test);
 	return 0;
-
-
 }
